6420414adde2a0d7f711b5c57365b668835ef14e
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-06-10 09:57:03 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,599 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-45_overview",
+      "name": "SF-45_Overview",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.30335077564211765,
+        "pitch": 0.6206146766327514,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.13231332683776564,
+          "pitch": 0.4869093650847631,
+          "rotation": 0,
+          "target": "1-sf-45_overview-wellhead"
+        },
+        {
+          "yaw": -0.17110025651662752,
+          "pitch": 1.1907494307367337,
+          "rotation": 0,
+          "target": "10-sf-45_concrete-pad"
+        },
+        {
+          "yaw": -1.1041801243819762,
+          "pitch": 0.4693405128055925,
+          "rotation": 0,
+          "target": "9-sf-45_waste-pit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sf-45_overview-wellhead",
+      "name": "SF-45_Overview wellhead",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.09527752752102359,
+        "pitch": 0.23037225930846716,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.189599241692747,
+          "pitch": -0.19086009701428885,
+          "rotation": 0,
+          "target": "0-sf-45_overview"
+        },
+        {
+          "yaw": -0.1552877325676576,
+          "pitch": 0.22440357519749554,
+          "rotation": 0,
+          "target": "2-sf-45_wellhead-top"
+        },
+        {
+          "yaw": -0.4424694311985906,
+          "pitch": 0.46304794096631596,
+          "rotation": 0,
+          "target": "4-sf-45_wellhead-s"
+        },
+        {
+          "yaw": -2.382490833151376,
+          "pitch": 0.15557116409873828,
+          "rotation": 0,
+          "target": "5-sf-45_well-pad"
+        },
+        {
+          "yaw": -0.2190731468694267,
+          "pitch": 0.12520352485900332,
+          "rotation": 0,
+          "target": "10-sf-45_concrete-pad"
+        },
+        {
+          "yaw": 1.0272481569266407,
+          "pitch": 0.07825080729637968,
+          "rotation": 0,
+          "target": "9-sf-45_waste-pit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sf-45_wellhead-top",
+      "name": "SF-45_Wellhead top",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8053995507413232,
+        "pitch": 0.7609209231660401,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.963251700286378,
+          "pitch": 0.3224707567138889,
+          "rotation": 0,
+          "target": "1-sf-45_overview-wellhead"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sf-45_wellhead-w",
+      "name": "SF-45_Wellhead W",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.11296244881897621,
+        "pitch": 0.04977914738981326,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6256858329697401,
+          "pitch": 0.30797149590481254,
+          "rotation": 0,
+          "target": "4-sf-45_wellhead-s"
+        },
+        {
+          "yaw": -3.029137875069459,
+          "pitch": 0.203218416036,
+          "rotation": 0,
+          "target": "10-sf-45_concrete-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sf-45_wellhead-s",
+      "name": "SF-45_Wellhead S",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.07502729809618813,
+        "pitch": 0.3634927447847325,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8530879396371667,
+          "pitch": 0.27202209830830704,
+          "rotation": 0,
+          "target": "3-sf-45_wellhead-w"
+        },
+        {
+          "yaw": 0.7171029943330645,
+          "pitch": 0.09531517457085847,
+          "rotation": 0,
+          "target": "1-sf-45_overview-wellhead"
+        },
+        {
+          "yaw": 2.514658765806286,
+          "pitch": 0.11645179566698438,
+          "rotation": 0,
+          "target": "5-sf-45_well-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sf-45_well-pad",
+      "name": "SF-45_Well Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.092446402481933,
+        "pitch": 0.27596718410619303,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7212847764636265,
+          "pitch": 0.059845705446296193,
+          "rotation": 0,
+          "target": "1-sf-45_overview-wellhead"
+        },
+        {
+          "yaw": -2.993123226632811,
+          "pitch": 0.22428753605025342,
+          "rotation": 0,
+          "target": "4-sf-45_wellhead-s"
+        },
+        {
+          "yaw": -0.693307530152584,
+          "pitch": 0.023815297139980984,
+          "rotation": 0,
+          "target": "6-sf-45_well-pad-1"
+        },
+        {
+          "yaw": 2.8063133275379393,
+          "pitch": 0.1318833873612384,
+          "rotation": 0,
+          "target": "10-sf-45_concrete-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sf-45_well-pad-1",
+      "name": "SF-45_Well Pad 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.3159247382374755,
+        "pitch": 0.1262635364239557,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9817954032401026,
+          "pitch": 0.08626404369841367,
+          "rotation": 0,
+          "target": "5-sf-45_well-pad"
+        },
+        {
+          "yaw": -1.431316972297008,
+          "pitch": 0.04630078350059463,
+          "rotation": 0,
+          "target": "7-sf-45_well-pad-ohl"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sf-45_well-pad-ohl",
+      "name": "SF-45_Well Pad OHL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.6895767398054673,
+        "pitch": -0.20028322903054807,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5435404274986233,
+          "pitch": 0.0845283507972745,
+          "rotation": 0,
+          "target": "6-sf-45_well-pad-1"
+        },
+        {
+          "yaw": 0.00998313144614471,
+          "pitch": 0.027760413503944648,
+          "rotation": 0,
+          "target": "8-sf-45_well-pad-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-sf-45_well-pad-2",
+      "name": "SF-45_Well Pad 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.2309157248118225,
+        "pitch": 0.055575481852198294,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.189466986441568,
+          "pitch": 0.08751640167920449,
+          "rotation": 0,
+          "target": "7-sf-45_well-pad-ohl"
+        },
+        {
+          "yaw": -0.9760795106020943,
+          "pitch": 0.007931068181136425,
+          "rotation": 0,
+          "target": "9-sf-45_waste-pit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sf-45_waste-pit",
+      "name": "SF-45_Waste pit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.09694538518047047,
+        "pitch": -0.04398956784019248,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1290284766480765,
+          "pitch": 0.09428761043760048,
+          "rotation": 0,
+          "target": "8-sf-45_well-pad-2"
+        },
+        {
+          "yaw": -2.4291657562112654,
+          "pitch": 0.045780988969745806,
+          "rotation": 0,
+          "target": "10-sf-45_concrete-pad"
+        },
+        {
+          "yaw": -2.8057165270405164,
+          "pitch": -0.0006679583843336445,
+          "rotation": 0,
+          "target": "1-sf-45_overview-wellhead"
+        },
+        {
+          "yaw": -2.217191202443898,
+          "pitch": -0.22924689440251989,
+          "rotation": 0,
+          "target": "0-sf-45_overview"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-sf-45_concrete-pad",
+      "name": "SF-45_Concrete Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.6325373359425193,
+        "pitch": 0.30561594599288355,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.134555704995484,
+          "pitch": 0.09904851429848094,
+          "rotation": 0,
+          "target": "9-sf-45_waste-pit"
+        },
+        {
+          "yaw": -2.7184360731149546,
+          "pitch": 0.24485112172655477,
+          "rotation": 0,
+          "target": "3-sf-45_wellhead-w"
+        },
+        {
+          "yaw": -2.721742695520991,
+          "pitch": -0.034675109700586404,
+          "rotation": 0,
+          "target": "1-sf-45_overview-wellhead"
+        },
+        {
+          "yaw": -1.8911123889168948,
+          "pitch": 0.14428643128151997,
+          "rotation": 0,
+          "target": "5-sf-45_well-pad"
+        },
+        {
+          "yaw": 0.18675824050279033,
+          "pitch": -0.19786358595596987,
+          "rotation": 0,
+          "target": "0-sf-45_overview"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WELL PAD SF-45",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
